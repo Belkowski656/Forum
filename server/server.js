1e621fe7a33b791a25c91b1aba1999778a2f21ec
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-09-10 19:35:24 +0200

    delete useCreateIndex

--- server/server.js
+++ server/server.js
@@ -23,7 +23,6 @@ try {
   mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   });
 } catch (err) {
   console.log(err);
